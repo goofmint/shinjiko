67b9b826bb7ecb2bc3f3e7ac56d2d10b9419e706
moongift <moongift@a85bf798-ba4d-0410-a545-bfaac6c6a6cb>
2008-06-11 08:00:23 +0000

    Google analytics config. configure files added.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -5,10 +5,16 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   include AuthenticatedSystem
   # If you want "remember me" functionality, add this before_filter to Application Controller
-  before_filter :login_from_cookie
+  before_filter :login_from_cookie, :config_load
   init_gettext "shinjiko"
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'c8fcef0f8ee0ed2d4c27db1d18a35778'
+  
+  def config_load
+    file = "#{RAILS_ROOT}/config/shinjiko.yml"
+    raise "Config file does not exits." unless File::exist?(file)
+    @shinjiko_config = YAML::load_file(file)[RAILS_ENV]
+  end
 end
